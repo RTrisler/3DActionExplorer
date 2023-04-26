0abeceae4af2dffa3199c5ec7c0f0a195effc944
njahn88 <nja010@email.latech.edu>
2023-04-26 02:06:03 -0500

    added particle effects for a health pick up

--- Assets/Scripts/STEPManager.cs
+++ Assets/Scripts/STEPManager.cs
@@ -17,10 +17,10 @@ public class STEPManager : MonoBehaviour
                 }
                 else
                 {
-                    _charge -= Time.deltaTime;
+                    _charge -= Time.deltaTime * 2;
                 }
-                //Debug.Log(_charge);
-                //Debug.Log(_currentStepState);
+                Debug.Log(_charge);
+                Debug.Log(_currentStepState);
                 switch (_currentStepState)
                 {
                     case StepState.Uncharged:
@@ -68,8 +68,8 @@ public class STEPManager : MonoBehaviour
                     _charge += Time.deltaTime;
                 }
                 _charge += Time.deltaTime;
-                //Debug.Log(_charge);
-                //Debug.Log(_currentStepState);
+                Debug.Log(_charge);
+                Debug.Log(_currentStepState);
                 switch (_currentStepState)
                 {
                     case StepState.Uncharged:
