b4abef7acc25e6d33fc5b8f0d924fc0283629682
njahn88 <nja010@email.latech.edu>
2023-04-26 21:33:29 -0500

    Fixed collision errors with collect

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -7,15 +7,18 @@ public class Collector : MonoBehaviour
     private RaycastHit hit;
     private PlayerManager playerManager;
 
+    [SerializeField]
+    private LayerMask canCollect;
+
     private void Start()
     {
         playerManager = GetComponentInParent<PlayerManager>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        Debug.DrawRay(transform.position, transform.forward * .05f, Color.green);
-        if (Physics.Raycast(transform.position, transform.forward * .05f, out hit, .05f))
+        Debug.DrawRay(transform.position - transform.forward * .5f, transform.forward * 1f, Color.green);
+        if (Physics.Raycast(transform.position - transform.forward * .5f, transform.forward * 1f, out hit, 1f, canCollect))
         {
             if (hit.transform.TryGetComponent<IPickupable>(out IPickupable pickup))
             {
