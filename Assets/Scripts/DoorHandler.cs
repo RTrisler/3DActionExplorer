b2cb6b8d1f9befd51014d8d46a8e53cc8e53661d
njahn88 <nja010@email.latech.edu>
2023-04-28 11:50:28 -0500

    added doors with simple animation

--- Assets/Scripts/DoorHandler.cs
+++ Assets/Scripts/DoorHandler.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class DoorHandler : MonoBehaviour, IInteractable
 {
+    [SerializeField]
+    private Animator _myAnim;
+
+    void Start()
+    {
+        _myAnim = GetComponent<Animator>();
+    }
     public void Interact()
     {
-        Destroy(gameObject);
+        _myAnim.SetBool("opening", true);
+    }
+
+    public void revertBool()
+    {
+        _myAnim.SetBool("opening", false);
     }
 }
