e798f89d33972979e94639361150967436d10d35
njahn88 <nja010@email.latech.edu>
2023-05-08 14:54:08 -0500

    puzzle can be fully completed

--- Assets/Scripts/PuzzleBall.cs
+++ Assets/Scripts/PuzzleBall.cs
@@ -6,8 +6,12 @@ public class PuzzleBall : MonoBehaviour
 {
     public Rigidbody _ballRigidBody;
 
+    [SerializeField]
+    public SphereCollider _collectCollider;
+
     private void Start()
     {
         _ballRigidBody = GetComponent<Rigidbody>();
+        _collectCollider.enabled = false;
     }
 }
