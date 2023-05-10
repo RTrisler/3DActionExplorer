859871394d6de37a348e97c662f08d5d8376a498
njahn88 <nja010@email.latech.edu>
2023-05-10 11:50:02 -0500

    a

---
+++ Assets/Scripts/JumpTracker.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTracker : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _floatEnemies;
+
+
+}
